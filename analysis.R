5037c755345d9ccae18eed88d723842243a66591
Kaylamchea <kayla900@uw.edu>
2019-11-15 16:35:27 -0800

    Created Plot

--- analysis.R
+++ analysis.R
@@ -1,4 +1,19 @@
 library("dplyr")
+library("ggmap")
+library("plotly")
 
 education_data <- read.csv("data/barro_lee_dataset.csv", stringsAsFactors = FALSE)
 
+## Percentage of no schooling in 2010 in each country. 
+locations <- education_data %>%
+  filter(year == "2010") %>%
+  select(country, lu)
+
+plot_geo(
+  type = 'choropleth',
+  locations = locations$country,
+  locationmode = 'country names',
+  colorscale = 'Viridis',
+  z = locations$lu
+)
+  
