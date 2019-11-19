aff1c9541a96c416eeb797d6526bb7549a2a566b
SinanZhao <cielzhao1013@gmail.com>
2019-11-19 14:21:59 -0800

    addVisualizations

--- docs/analysis.R
+++ docs/analysis.R
@@ -1,13 +1,29 @@
+# Load ggplot2, and dplyr libraries
+library("ggplot2")
 library("dplyr")
-library("ggmap")
-library("plotly")
 
-education_data <- read.csv("data/barro_lee_dataset.csv", stringsAsFactors = FALSE)
+# Read the data
+education_data <- read.csv("../data/barro_lee_dataset.csv", stringsAsFactors = FALSE)
+
+# Plot
+get_specific_lpc <- function(code) {
+  lpc <- education_data %>%
+    filter(year == 2010) %>%
+    select(year, country, lpc, region_code) %>%
+    group_by(country) %>%
+    summarise(lpc = sum(lpc),
+              region_code = region_code) %>%
+    filter(region_code == code)
+}
+
+lpc_region <- get_specific_lpc("Middle East and North Africa")
+
 
 ## Percentage of no schooling in 2010 in each country. 
 locations <- education_data %>%
   filter(year == "2010") %>%
   select(country, lu)
+
 
 plot_geo(
   type = 'choropleth',
@@ -16,4 +32,3 @@ plot_geo(
   colorscale = 'Viridis',
   z = locations$lu
 )
-  
--- docs/app.R
+++ docs/app.R
@@ -1,6 +1,19 @@
+source("m.R")
+
+# Load the shiny, ggplot2, and dplyr libraries
+library("shiny")
+library("ggplot2")
+library("dplyr")
+
+# Read the file
+
 page_one <- tabPanel(
   "Background & Research Questions", # label for the tab in the navbar
-  titlePanel("Background & Research Questions"), # show with a displayed title
+  titlePanel("Page 1"), # show with a displayed title
+  
+  # This content uses a sidebar layout
+  
+ # show with a displayed title
   
   # This content uses a sidebar layout
   p("Our project is ....")
@@ -8,8 +21,25 @@ page_one <- tabPanel(
 
 # Define content for the second page
 page_two <- tabPanel(
-  "Visualization" # label for the tab in the navbar
-  # ...more content would go here...
+  "Visualization",
+  titlePanel("The percentage of primary school completion per country based on different region in 2010"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Show user the percentage of primary complete per country based on different region"),
+      
+      selectInput("region",
+                  label = "Choose a region",
+                  choices = unique(education_data$region_code),
+                  selected = "Middle East and North Africa"
+      )
+    ),
+    
+    mainPanel(
+      plotOutput("plot"),
+      plotlyOutput("map")
+    )
+  )
 )
 
 # Define content for the third page
@@ -30,15 +60,37 @@ page_five <- tabPanel(
   # ...more content would go here...
 )
 
-ui <- navbarPage(
-  "Youth Education Across the World", # application title
+ui <- fluidPage(
+  titlePanel("Youth Education Across the World"), # application title
+  tabsetPanel(
   page_one,         # include the first page content
   page_two,         # include the second page content
   page_three,       # include the third page content
-  page_four,
-  page_five
+  page_four,        # include the four page content
+  page_five         # include the five page content 
+  )
 )
 
+
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    ggplot(get_specific_lpc(input$region), 
+           aes(x = abbreviate(country), y = lpc, fill = country)
+    ) +
+    geom_col()
+  })
+  output$map <- renderPlotly({
+    map <- plot_geo(
+      type = 'choropleth',
+      locations = locations$country,
+      locationmode = 'country names',
+      colorscale = 'Viridis',
+      z = locations$lu
+    )  
+    return(map)
+  })
+  
+}
 
 # Pass each page to a multi-page layout (`navbarPage`)
 
